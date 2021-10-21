bc96790b6788a14cc2080d1ef9f45777dc8529db
AndrewJackson22 <mihailov.911@list.ru>
2021-10-21 17:26:57 +0300

    first

---
+++ NavigationTable-2/Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Maria Mezhova on 23.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    private let tableView = UITableView(frame: .zero, style: .grouped)
+    private let cellId = "cellId"
+    private let arrayOfPosts = PostWall.tableViewPost
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpTableView()
+    }
+    
+    private func setUpTableView() {
+        view.addSubview(tableView)
+        tableView.toAutoLayout()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: cellId)
+       
+        let constraints = [
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+// MARK: UITableViewDataSource
+
+extension ProfileViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfPosts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: PostTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! PostTableViewCell
+        
+        cell.post = arrayOfPosts[indexPath.row]
+        
+        return cell
+    }
+}
+
+// MARK: UITableViewDelegate
+
+extension ProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        switch section {
+        case 0: let headerView = ProfileTableHeaderView()
+            return headerView
+        default:
+            return nil
+        }
+       
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
